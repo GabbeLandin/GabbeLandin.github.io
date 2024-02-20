1fde626d4d734cec8911ec682fa98f43e8484fc8
gablan <gablan@kth.se>
2024-02-20 17:07:35 +0100

    Website css work

--- src/App.js
+++ src/App.js
@@ -18,33 +18,47 @@ import { slide as Menu } from 'react-burger-menu';
 
 function App() {
   return (
-    <div className="flex-container">
-      
-        <header className='header'>
-          <img src={logo} alt="Logo" className="logo-image" />
-          <ul>
-            <li><a href="default.asp">Home</a></li>
-            <li><a href="news.asp">News</a></li>
-            <li><a href="contact.asp">Contact</a></li>
-            <li><a href="about.asp">About</a></li>
-          </ul>
-        </header>
-        <div className="main">
-          <div className='main-text'>
-              <div className='paragraph'>
-                Jag heter Ingrid Landin och är en erfaren psykolog med över tjugo års erfarenhet inom området. Mitt specialområde är kognitiv beteendeterapi (KBT) och mindfulnessbaserade metoder. Jag brinner för att hjälpa människor att hantera ångest, depression och stress. I mina terapisessioner fokuserar jag på dig som individ och anpassar mina metoder efter dina behov. Min empatiska inställning och evidensbaserade tekniker har gjort att jag har fått ett gott rykte för att främja positiv förändring och hjälpa mina klienter att leva meningsfulla liv.
-              </div>
-              <img src={ingrid} alt="test" className="ingrid" />
+    <div>
+      <head></head>
+      <body>
+          <header className='header'>
+            <img src={logo} alt="Logo" className="logo-image" />
+            <ul>
+              <li><a href="default.asp">Home</a></li>
+              <li><a href="news.asp">News</a></li>
+              <li><a href="contact.asp">Contact</a></li>
+              <li><a href="about.asp">About</a></li>
+            </ul>
+          </header>
+          <div className="main">
+            <div className='bigtext'>
+              <h2>
+                Medkänslofokuserad psykoterapi;
+              </h2>
+              <h2>
+                Compassion Focused Therapy - CFT
+                </h2>
+            </div>
+            
+            <div className='main-text'>
+                <div className='paragraph'>
+                  <h2 className='rubrik'>
+                      Vem är jag
+                  </h2>
+                  Jag heter Ingrid Landin och är en erfaren psykolog med över tjugo års erfarenhet inom området. Mitt specialområde är kognitiv beteendeterapi (KBT) och mindfulnessbaserade metoder. Jag brinner för att hjälpa människor att hantera ångest, depression och stress. I mina terapisessioner fokuserar jag på dig som individ och anpassar mina metoder efter dina behov. Min empatiska inställning och evidensbaserade tekniker har gjort att jag har fått ett gott rykte för att främja positiv förändring och hjälpa mina klienter att leva meningsfulla liv.
+                </div>
+                <img src={ingrid} alt="test"/>
+            </div>
           </div>
-        </div>
-        <footer className='footer'>
-          <div><strong>Adress | </strong>potatisgatan 1337</div>
-          <div><strong>Telefon | </strong>133713371337</div>
-          <div><strong>E-post | </strong>potatis42@1337.com</div>
-          <p>&copy; 2024 Dr. Ingrid Landin's Practice. All rights reserved.</p>
-        </footer>
-      
-      
+          <footer className='footer'>
+            <div><strong>Adress | </strong>potatisgatan 1337</div>
+            <div><strong>Telefon | </strong>133713371337</div>
+            <div><strong>E-post | </strong>potatis42@1337.com</div>
+            <p>&copy; 2024 Dr. Ingrid Landin's Practice. All rights reserved.</p>
+          </footer>
+        
+        
+      </body>
     </div>
   );
 }
